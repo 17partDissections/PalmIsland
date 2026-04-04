94e2aa7e05d52bb5494494f16b859614372b7cdd
Andrew <159803151+17partDissections@users.noreply.github.com>
2026-04-04 18:34:26 +0300

    Changed Game scene installer

--- PalmIsland/Assets/Scripts/Infrastructure/GameSceneInstaller.cs
+++ PalmIsland/Assets/Scripts/Infrastructure/GameSceneInstaller.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Unity.Cinemachine;
 using UnityEngine;
 using Zenject;
@@ -8,6 +9,8 @@ namespace Q17pD.PalmIsland.Infrastructure
     {
         [SerializeField] private AudioHandler _audioHandler;
         [SerializeField] private CinemachineBrain _cinemachineBrain;
+        [SerializeField] private Player.Player _playerInstance;
+        [SerializeField] private List<Palm> _palms;
 
         public override void InstallBindings()
         {
@@ -20,6 +23,11 @@ namespace Q17pD.PalmIsland.Infrastructure
                 .FromInstance(_audioHandler)
                 .AsSingle()
                 .NonLazy();
+        }
+        public void Awake()
+        {
+            _playerInstance.Init(_audioHandler, _cinemachineBrain);
+            foreach (var palm in _palms) palm.Init(_playerInstance);
         }
     }
 }
