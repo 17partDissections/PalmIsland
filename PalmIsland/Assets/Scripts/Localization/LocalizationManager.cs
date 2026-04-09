7c7950bcb44b2490261a97397c7b94e628725ab3
Andrew <159803151+17partDissections@users.noreply.github.com>
2026-04-09 18:47:01 +0300

    Updated LocalizationManager.cs to latest version(Frostwatch)

--- PalmIsland/Assets/Scripts/Localization/LocalizationManager.cs
+++ PalmIsland/Assets/Scripts/Localization/LocalizationManager.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using DFTGames.Localization;
+using System.ComponentModel;
 
-namespace MBW.WaST
+namespace Q17pD.PalmIsland
 {
     public class LocalizationManager : MonoBehaviour
     {
@@ -19,16 +20,30 @@ namespace MBW.WaST
             SystemLanguage.Ukrainian,
         };
         private int _languageIndex;
+        private int _lastLanguageIndex = -1;
 
         private void Start() { _languageIndex = PlayerPrefs.GetInt("LangIndex", 0); }
         public void ChangeIndex(int newIndex)
         {
+            if (_lastLanguageIndex == -1)
+                _lastLanguageIndex = PlayerPrefs.GetInt("LangIndex", 0);
             _languageIndex = newIndex;
+            ChangeLanguage();
         }
         public void ChangeLanguage() { Localize.SetCurrentLanguage(_languages[_languageIndex]); }
         public void CompletelyChangeLanguage()
         {
             PlayerPrefs.SetInt("LangIndex", _languageIndex);
+            _lastLanguageIndex = -1;
+            ChangeLanguage();
+        }
+        public void Revert()
+        {
+            if (_lastLanguageIndex != -1)
+            {
+                _languageIndex = _lastLanguageIndex;
+                _lastLanguageIndex = -1;
+            }
             ChangeLanguage();
         }
     }
